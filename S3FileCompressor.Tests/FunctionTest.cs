353a3d271441b034563c5e406bdab46d391899df
Ross Huelin <filmstarr@hotmail.com>
2023-02-03 10:36:53 +0000

    Update packages and update to .NET 6.

--- S3FileCompressor.Tests/FunctionTest.cs
+++ S3FileCompressor.Tests/FunctionTest.cs
@@ -9,6 +9,7 @@ using Amazon;
 using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Util;
+using static Amazon.Lambda.S3Events.S3Event;
 
 namespace S3FileCompressor.Tests
 {
@@ -36,14 +37,14 @@ namespace S3FileCompressor.Tests
                 // Setup the S3 event object that S3 notifications would create with the fields used by the Lambda function.
                 var s3Event = new S3Event
                 {
-                    Records = new List<S3EventNotification.S3EventNotificationRecord>
+                    Records = new List<S3EventNotificationRecord>
                     {
-                        new S3EventNotification.S3EventNotificationRecord
+                        new S3EventNotificationRecord
                         {
-                            S3 = new S3EventNotification.S3Entity
+                            S3 = new S3Entity
                             {
-                                Bucket = new S3EventNotification.S3BucketEntity {Name = bucketName },
-                                Object = new S3EventNotification.S3ObjectEntity {Key = key }
+                                Bucket = new S3BucketEntity {Name = bucketName },
+                                Object = new S3ObjectEntity {Key = key }
                             }
                         }
                     }
